b402cd55b575d1110fd69272180fad769bcc1d89
Mr Z <denver.cfman@gmail.com>
2024-07-29 22:31:27 -0600

    Update usbmount.sh

--- usbmount.sh
+++ usbmount.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Detected new device: $1" >>/var/log/PiBAN.log
+/usr/bin/mosquitto_pub -h $mqtt_server -p 1883 -u $CHIPWIPE_USER -P $CHIPWIPE_PASS -t chip/piBAN -m "Detected new device: $1 on chip" || True
 devname=$(basename $1)
 logname=/tmp/$devname.log
 if [ "${ACTION}" = "add" ]
