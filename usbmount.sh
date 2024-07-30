eda518bf5c070a4430f794ccf2ba525af247c524
Mr Z <denver.cfman@gmail.com>
2024-07-29 22:43:00 -0600

    Update usbmount.sh

--- usbmount.sh
+++ usbmount.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 mqtt_server=10.0.50.47
+CHIPWIPE_USER=changeme
+CHIPWIPE_PASS=changeme
+
 echo "Detected new device: $1" >>/var/log/PiBAN.log
 /usr/bin/mosquitto_pub -h $mqtt_server -p 1883 -u $CHIPWIPE_USER -P $CHIPWIPE_PASS -t chip/piBAN -m "Detected new device: $1 on chip" || True
 devname=$(basename $1)
